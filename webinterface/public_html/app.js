b60ddd90140019ee754042c0bf84af6aea518d9f
Jens True <jens.chr.true@gmail.com>
2018-10-03 17:49:48 +0200

    Adding graphs and table output

--- webinterface/public_html/app.js
+++ webinterface/public_html/app.js
@@ -1,16 +1,36 @@
+var ctx = document.getElementById("chart_voltage").getContext('2d');
+var chart = new Chart(ctx, {"type": "line", "data": {"labels": [], "datasets": [{"label": "Voltage", "data": [], "fill": false, "borderColor": "rgb(75, 192, 192)", "lineTension": 0.1}]}, "options": {}});
+function addDataChart(chart,  label, data) {
+    chart.data.labels.push(label.toUTCString());
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+counter = 1;
+function addDataTable( label, data) {
+        var markup = "<tr>"
+        markup += "<th scope='row'>"+counter+"</th>";
+        markup +="<td>"+label.toUTCString()+"</td>";
+        markup += "<td>"+data+ " V</td>";
+        markup += "</tr>";
+        $("#table_voltage tbody").append(markup);
+        counter += 1;
+}
+
 // Create a client instance
-client = new Paho.MQTT.Client(mqtt_url, Math.random().toString(36).substring(20));
+var client = new Paho.MQTT.Client(mqtt_url, Math.random().toString(36).substring(20));
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
-
 // connect the client
 client.connect({onSuccess:onConnect});
 
 function set_state(state) {
     console.log("Set state to: ", state);
-    message = new Paho.MQTT.Message("state");
+    message = new Paho.MQTT.Message(state);
     message.destinationName = prefix + device_id + "/measure/state/set";
     client.send(message);
 }
@@ -41,6 +61,7 @@ function onConnect() {
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
+  $( "#connection_status" ).text("Conenction lost");
   if (responseObject.errorCode !== 0) {
     console.log("onConnectionLost:"+responseObject.errorMessage);
   }
@@ -67,11 +88,15 @@ function onMessageArrived(message) {
     }
     if(message.destinationName === prefix + device_id + "/measure/voltage")  {
            $( "#reading_voltage" ).text(message.payloadString); 
+           addDataChart(chart, new Date(), message.payloadString);
+           addDataTable(new Date(), message.payloadString);
     }
     if(message.destinationName === prefix + device_id + "/measure/current")  {
            $( "#reading_current" ).text(message.payloadString); 
     }
-    if(message.destinationName === prefix + device_id + "/measure/capacity")  {
+    if(message.destinationName === prefix + device_id + "/measure/charge")  {
            $( "#reading_capacity" ).text(message.payloadString); 
     }
 }
+
+

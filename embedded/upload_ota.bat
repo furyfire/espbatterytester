a439f33ec6f1572f4adeb89af9b48193d0276d53
Jens True <furyfuryakaff@gmail.com>
2018-10-30 22:49:52 +0100

    Added device id as param

--- embedded/upload_ota.bat
+++ embedded/upload_ota.bat
@@ -1,1 +1,1 @@
-python .piolibdeps\Homie\scripts\ota_updater\ota_updater.py -l jcktrue.dk -i 18fe34a28bcc .pioenvs\nodemcu\firmware.bin
+python .piolibdeps\Homie\scripts\ota_updater\ota_updater.py -l jcktrue.dk -i $1 .pioenvs\nodemcu\firmware.bin
